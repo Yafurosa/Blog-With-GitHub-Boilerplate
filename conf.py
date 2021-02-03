7b9f4848b183b0c1fe3b06f860e3263201633c31
Yafurosa <78487537+Yafurosa@users.noreply.github.com>
2021-02-04 00:04:34 +0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -21,7 +21,7 @@ enable_jsdelivr = {
 # 站点设置
 site_name = "Nostrum"
 site_logo = "${static_prefix}logo.png"
-site_build_date = "2019-12-18T16:51+08:00"
+site_build_date = "2020-18T16:51+08:00"
 author = "Yafurosa"
 email = "nostrum0@163.com"
 author_homepage = "https://www.imalan.cn"
