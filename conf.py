e3ea04c32e31097cdfac8cd2337c13f9a4de57a5
Yafurosa <78487537+Yafurosa@users.noreply.github.com>
2021-02-03 23:09:03 +0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -19,7 +19,7 @@ enable_jsdelivr = {
 }
 
 # 站点设置
-site_name = "Nostrumpod"
+site_name = "Nostrum"
 site_logo = "${static_prefix}logo.png"
 site_build_date = "2019-12-18T16:51+08:00"
 author = "Yafurosa"
